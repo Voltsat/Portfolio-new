6d735938115e9bdf49eb2359c6b54ff0ab0acafc
Voltsat <net71727@gmail.com>
2025-08-12 22:32:11 +0700

    c

---
+++ js/skills.js
@@ -0,0 +1,7 @@
+function mobMenu() {
+    const hamburger = document.getElementById("hamburgerID");
+    const navbar = document.getElementById("navMenu");
+
+    hamburger.classList.toggle("active");
+    navbar.classList.toggle("active");
+}
